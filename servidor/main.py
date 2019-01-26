a97a18b4c0d4554462c365a2b3e14aee537c394c
Fernando Lucena Calixto <fer.lc.developer@gmail.com>
2019-01-26 12:54:12 -0600

    token

--- servidor/main.py
+++ servidor/main.py
@@ -1,17 +1,28 @@
 from flask import Flask 
-
+import jwt
+from flask_restful import Api, Resource, reqparse
 
 app = Flask(__name__)
-
+api = Api(app)
 
 @app.route('/')
 
-
-def hello_world():
-    """ Example """
-    return "hola mundo"
+class EncToken(Resource):
+    def get(self):
+        """ Example """
+        encoded = jwt.encode( { 'some' : 'payload' }, 'secret', algorithm = 'HS256' )
+        return encoded, 202
+    
+class DecToken(Resource):
+    def get(self):
+        """ Example """
+        encoded = jwt.encode( { 'some' : 'payload' }, 'secret', algorithm = 'HS256' )
+        decode = jwt.decode( encoded, 'secret', algorithms=['HS256'])
+        return decode, 202
 
 
 if __name__ == '__main__':
-    help(hello_world)
-    app.run(host='127.0.0.1', port=80)
+    #help(hello_world)
+    api.add_resource(EncToken,'/token-enc')
+    api.add_resource(DecToken,'/token-dec')
+    app.run(host='127.0.0.1', port=8081)
